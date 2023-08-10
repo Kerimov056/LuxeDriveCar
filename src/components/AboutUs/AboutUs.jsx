5dba9c5186bb77d80f09f8128c92b479599cd5c9
Kerimov056 <ulvisk@code.edu.az>
2023-08-10 12:15:09 -0700

    AboutUs next

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,7 +3,7 @@ import './aboutus.scss'
 import Navbar from '../Navbar/Navbar'
 import ImageCom from '../Imagecompanents/ImageCom'
 import Destinations from '../Destinations/Destinations'
-
+import Imagecompanents from '../Imagecompanents/Imagecompanents'
 
 const AboutUs = () => {
 
@@ -13,7 +13,7 @@ const AboutUs = () => {
         <Navbar />
         <ImageCom img={"https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/02/inner-ab-img-1.jpg"} />
         <Destinations name={"We Value Our Clients And  Want Them To Have A Nice Experience"} topic={"FINEST TRANSPORT"} isAnswer={true} isHuman={false} color={"white"} />
-
+        <Imagecompanents img={"https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/01/h1-img-8.jpg"} />
       </div>
     </>
   )
