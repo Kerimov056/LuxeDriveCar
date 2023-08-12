0d84291a91d8af1a0d41a7af4e7761e5130e1bd7
Kerimov056 <ulvisk@code.edu.az>
2023-08-12 00:24:16 -0700

    utils && service Auth Login start

--- src/components/Services/authServices.js
+++ src/components/Services/authServices.js
@@ -1,1 +1,12 @@
-//salam
+import { httpClient } from "../utils/HttpClient";
+
+export const login=(data) =>{
+    return httpClient.post('api/Auth/Login',data)
+}
+
+
+
+
+
+
+// https://localhost:7152/api/Auth/Login
