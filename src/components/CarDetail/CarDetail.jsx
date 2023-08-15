9bd6f54bc16cce51241f15b91cdb0597c98d3b75
Kerimov056 <ulvisk@code.edu.az>
2023-08-15 14:11:51 -0700

    Car Details

--- src/components/CarDetail/CarDetail.jsx
+++ src/components/CarDetail/CarDetail.jsx
@@ -22,17 +22,19 @@ const CarDetail = () => {
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non id est laborum ultrices tellus, in suscipit massa vehicula eu.</p>
                             <div className='addCart'>
                                 <div>
-                                    <div>8</div>
-                                    <div>
+                                    <div className='Sum'>8</div>
+                                    <div className='hesab'>
                                         <div>+</div>
                                         <div>-</div>
                                     </div>
                                 </div>
-                                <button>+ADD TO CART</button>
+                                <button>+ ADD TO CART</button>
                             </div>
 
                             <div className='Det'>
-
+                                <div><span>SKU:</span><span>0058</span></div>
+                                <div><span>Catagory:</span><span>EXPENSIVE</span></div>
+                                <div><span>Tags:</span><span><button>#Car</button></span></div>
                             </div>
                         </div>
                     </div>
