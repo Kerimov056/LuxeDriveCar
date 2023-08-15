b08d7235f4e6e63fc60a882623c5bc758729f466
Kerimov056 <ulvisk@code.edu.az>
2023-08-14 18:43:36 -0700

    Fleet Structur

---
+++ src/components/BlogShopSturuktur/Strutur.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Strutur = () => {
+    return (
+        <>
+            <div id='Strutur'> 
+                <div>
+                    <div className='LeftBar'></div>
+                    <div className='RightBar'></div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Strutur;
