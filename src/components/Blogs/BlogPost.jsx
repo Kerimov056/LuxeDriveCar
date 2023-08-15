97844b6373ee9abb016abd5dc956d80c4ce56ac5
Kerimov056 <ulvisk@code.edu.az>
2023-08-15 10:44:56 -0700

    Blog page next Blog Post

--- src/components/Blogs/BlogPost.jsx
+++ src/components/Blogs/BlogPost.jsx
@@ -5,17 +5,49 @@ const BlogPost = () => {
     return (
         <>
             <div id='BlogPost'>
-                <div>
+                <div className='postName'>
                     <h1>What Are The Benefits Of Hiring A Private Driver And Traveling	</h1>
                     <h4>Jan 8.   2022./   DRIVE, LUXURY</h4>
                 </div>
-                <div>
-                    <img src='https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/02/b-l-img-1.jpg' />
+                <div className='postImg'>
+                    <div class="container noselect">
+                        <div class="canvas">
+                            <div class="tracker tr-1"></div>
+                            <div class="tracker tr-2"></div>
+                            <div class="tracker tr-3"></div>
+                            <div class="tracker tr-4"></div>
+                            <div class="tracker tr-5"></div>
+                            <div class="tracker tr-6"></div>
+                            <div class="tracker tr-7"></div>
+                            <div class="tracker tr-8"></div>
+                            <div class="tracker tr-9"></div>
+                            <div class="tracker tr-10"></div>
+                            <div class="tracker tr-11"></div>
+                            <div class="tracker tr-12"></div>
+                            <div class="tracker tr-13"></div>
+                            <div class="tracker tr-14"></div>
+                            <div class="tracker tr-15"></div>
+                            <div class="tracker tr-16"></div>
+                            <div class="tracker tr-17"></div>
+                            <div class="tracker tr-18"></div>
+                            <div class="tracker tr-19"></div>
+                            <div class="tracker tr-20"></div>
+                            <div class="tracker tr-21"></div>
+                            <div class="tracker tr-22"></div>
+                            <div class="tracker tr-23"></div>
+                            <div class="tracker tr-24"></div>
+                            <div class="tracker tr-25"></div>
+                            <div id="card">
+                                <img src='https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/02/b-l-img-1.jpg' />
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
+                <div className='postDesc'>
                     <h4>Nisl condimentum id venenatis a condimentum vitae sapien pellent esque habitant. Massa id neque aliquam vestibulum. Diam quam nulla porttitor massa id neque aliquam. Tortor at auctor urna nunc id cursus metus aliquam eleifend. At tellus at urna condimentum mattis pellentesque. Tristique sollicitudin nibh sit amet commodo nulla. Erat nam at lectus urna duis convallis. Vestibulum lectus mauris ultrices eros in cursus turpis. Volutpat commodo</h4>
                 </div>
-                <div>
+                <div className='postButton'>
                     <button>+ DISCOVER</button>
                 </div>
             </div>
