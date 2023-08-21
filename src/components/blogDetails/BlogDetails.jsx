9caa086d676c6fcb5b26cbaa3f2285bc55a32cbc
Kerimov056 <ulvisk@code.edu.az>
2023-08-21 09:52:36 -0700

    BlogDetails

--- src/components/blogDetails/BlogDetails.jsx
+++ src/components/blogDetails/BlogDetails.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './blogDetails.scss'
 import Strutur from '../BlogShopSturuktur/Strutur'
-
+import Navbar from '../Navbar/Navbar'
 
 const BlogDetails = () => {
   return (
     <>
+        <Navbar />
         <Strutur details={true} />
     </>
   )
