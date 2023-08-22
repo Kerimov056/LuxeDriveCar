90027bf7728841003e317f76f8e85741cd9b5917
Kerimov056 <ulvisk@code.edu.az>
2023-08-22 04:48:21 -0700

    Basket Car Service

--- src/components/Shop/ShopCarCard.js
+++ src/components/Shop/ShopCarCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './ShopCarCard.scss'
 import { Link } from 'react-router-dom';
+import { PostCar } from "../Services/basketServices";
 
 
 const ShopCarCard = (props) => {
+
+    const handleAddToOrder = () => {
+        PostCar(props.Id)
+            .then(response => {
+                console.log("Car added to order", response.data);
+            })
+            .catch(error => {
+                console.error("Error adding car to order", error);
+            });
+    }
+
     return (
         <>
             <div class="cardDDD">
@@ -11,7 +23,7 @@ const ShopCarCard = (props) => {
                     <img src={props.img} />
                 </div>
                 <button class="card-button"><Link to={`/CarDetail/${props.Id}`}>More Info</Link></button>
-                <button class="AddToCard">+ ADD TO ORDER</button>
+                <button onClick={handleAddToOrder} class="AddToCard">+ ADD TO ORDER</button>
             </div>
         </>
     );
