e73ef5e88850d54cfc95db8ab8417262c5f62b66
Kerimov056 <ulvisk@code.edu.az>
2023-08-29 01:11:57 -0700

    Home Page Responsive next

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -219,7 +219,7 @@ const Home = ({ color, onNavStateChange }) => {
             </div>
           </div>
         </div>
-        <div className='cards'>
+        <div className='cards' id='CardsRes'>
           {cars?.data.map((byCar, index) => (  //https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/02/Main-home-vehicle-list-img-01.jpg
             console.log("Imageeeeee", byCar.carImages[0]?.imagePath),
             <Card key={index} Id={byCar.id} img={`data:image/jpeg;base64,${byCar.carImages[0]?.imagePath}`} catagorie={byCar.carCategory ? byCar.carCategory.category : "No Category"} name={byCar.model} price={byCar.price} description={byCar.description.slice(0, 60)} />
@@ -275,7 +275,7 @@ const Home = ({ color, onNavStateChange }) => {
       <div id='Convenience'>
         <div>
           <div className='ConvenienceLeft'>
-            <h1>We Care Of Your Safety <br /> And Convenience</h1>
+            <h1 id='ConvenReserv'>We Care Of Your Safety <br /> And Convenience</h1>
             <h4>PREMIUM DRIVERS</h4>
             <p>Facilisi cras fermentum odio eu feugiat. In fermentum et sollicitudin ac ori accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Aenean euismod elementum nisi quis eleifend quam adipiscing vitae dipiscing</p>
             <button> + BOOK NOW </button>
