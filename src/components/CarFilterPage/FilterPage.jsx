ef7a5e1673acc1ac6ddc9deab6747748b6745054
Kerimov056 <ulvisk@code.edu.az>
2023-08-30 02:02:45 -0700

    FilterPage

--- src/components/CarFilterPage/FilterPage.jsx
+++ src/components/CarFilterPage/FilterPage.jsx
@@ -3,6 +3,7 @@ import "./filterpage.scss";
 import Nav from "../Navbar/Nav";
 import Navbartwo from "../Navbar/Navbartwo";
 import { Input } from '@chakra-ui/react';
+import {MdYoutubeSearchedFor} from "react-icons/md";
 
 const FilterPage = () => {
     return (
@@ -21,9 +22,7 @@ const FilterPage = () => {
                         <div>
                             <Input type='text' />
                             <button class="buttonSearcCar">
-                                <span>
-                                    <svg viewBox="0 0 24 24" height="24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="M9.145 18.29c-5.042 0-9.145-4.102-9.145-9.145s4.103-9.145 9.145-9.145 9.145 4.103 9.145 9.145-4.102 9.145-9.145 9.145zm0-15.167c-3.321 0-6.022 2.702-6.022 6.022s2.702 6.022 6.022 6.022 6.023-2.702 6.023-6.022-2.702-6.022-6.023-6.022zm9.263 12.443c-.817 1.176-1.852 2.188-3.046 2.981l5.452 5.453 3.014-3.013-5.42-5.421z"></path></svg>
-                                </span>
+                                <MdYoutubeSearchedFor />
                             </button>
                         </div>
                         <p>If you are not happy with the results below please do another search</p>
