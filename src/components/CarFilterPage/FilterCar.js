8dac6dd0f24f15269ed9168037b56d9cff05073b
Kerimov056 <ulvisk@code.edu.az>
2023-08-30 02:07:55 -0700

    width: 100%;

--- src/components/CarFilterPage/FilterCar.js
+++ src/components/CarFilterPage/FilterCar.js
@@ -4,7 +4,15 @@ import './filtercar.scss'
 const FilterCar = () => {
     return (
         <>
-
+            <div className='SearchCarCard'>
+                <div className='SearchCarCardImg'>
+                    <img src='https://luxedrive.qodeinteractive.com/wp-content/uploads/2023/01/Vihecle-single-featured-img-150x150.jpg' />
+                </div>
+                <div className='SearchCarCardText'>
+                    <h3>Bmw X2 M35i</h3>
+                    <p>Rolls Royce Overview Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo laborum consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla est pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id. Sagittis molestie […]</p>
+                </div>
+            </div>
         </>
     )
 }
