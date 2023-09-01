06c60101d595ecd9f5622b1832dea0b8d6aed895
Kerimov056 <ulvisk@code.edu.az>
2023-09-01 06:51:05 -0700

    leaflet and  leafelt draw

--- src/components/Map/Maps.jsx
+++ src/components/Map/Maps.jsx
@@ -1,10 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './map.scss'
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
 import 'leaflet/dist/leaflet.css';
+import "leaflet-draw/dist/leaflet.draw.css";
 import L from "leaflet";
 import useGeoLocation from './useGeoLocation';
- 
+
+
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+});
 
 const markerIcon = new L.Icon({
     iconUrl: require("./download.png"),
@@ -14,9 +29,11 @@ const markerIcon = new L.Icon({
 });
 
 const Maps = () => {
-    const [center, setCenter] = useState({ lat: 40.4093, lng: 49.8671});
+    const [center, setCenter] = useState({ lat: 40.4093, lng: 49.8671 });
 
-    const location =useGeoLocation();
+    const location = useGeoLocation();
+
+    const _created = (e) => console.log(e);
 
     return (
         <>
@@ -26,9 +43,11 @@ const Maps = () => {
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                         url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=S3UF58mBkVoHt2UkKpEL"
                     />
-
+                    <FeatureGroup>
+                        <EditControl position='topright' onCreated={_created} draw={{ rectangle: false, circlemarker: false, polygon: false }} />
+                    </FeatureGroup>
                     <Marker position={center} icon={markerIcon}>
-                        <Popup> 
+                        <Popup>
                             A pretty CSS3 popup. <br /> Easily customizable.
                         </Popup>
                     </Marker>
