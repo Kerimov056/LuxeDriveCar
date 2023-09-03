ca3d07ade41a830dc5104becb7da464969e5c890
Kerimov056 <ulvisk@code.edu.az>
2023-09-03 23:44:05 +0400

    Map to Transfer CarDetails

--- src/components/CarDetail/CarDetail.jsx
+++ src/components/CarDetail/CarDetail.jsx
@@ -12,7 +12,6 @@ import {
     useDisclosure, Input, Text,
     Button, FormLabel, FormControl
 } from '@chakra-ui/react'
-
 import Accardion from './Accardion';
 import ShopCarCard from '../Shop/ShopCarCard';
 import 'leaflet/dist/leaflet.css'
@@ -28,11 +27,96 @@ import { PostCar } from "../Services/basketServices";
 import ChauffeursCard from '../Chauffeurs/ChauffeursCard';
 import { getChauffeurs } from "../Services/chauffeursServices";
 import axios from 'axios';
+//---LeafLet
+import L from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import "leaflet-draw/dist/leaflet.draw.css";
+import { MapContainer, TileLayer, Marker, Popup, FeatureGroup } from "react-leaflet";
+import { EditControl } from "react-leaflet-draw";
+import '../Map/map.scss'
 
+
+const markerIcon = new L.Icon({
+    iconUrl: require("./download.png"),
+    iconSize: [45, 35],
+    iconAnchor: [17, 46],
+    popupAnchor: [0, -46],
+});
+
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    iconUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png",
+    shadowUrl:
+        "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png",
+});
 
 
 
 const CarDetail = () => {
+
+    //---------leaflet
+
+    const [center, setCenter] = useState({ lat: 40.4093, lng: 49.8671 });
+    const mapRef = useRef();
+
+    const [userLocation, setUserLocation] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true);
+    };
+
+    const closeModal = () => {
+        setShowModal(false);
+        setUserLocation(null);
+    };
+
+    const handleMapClick = () => {
+        openModal();
+    };
+
+    const shareLocation = () => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setUserLocation({ lat: latitude, lng: longitude });
+                },
+                (error) => {
+                    console.error("Konum alınamadı: ", error);
+                }
+            );
+        } else {
+            alert("Tarayıcınız konum hizmetini desteklemiyor.");
+        }
+        closeModal();
+    };
+
+    const [data, setData] = useState('Veri A');
+
+    const updateData = (newData) => {
+        setData(newData);
+    };
+
+    const handleShapeCreated = (e) => {
+        const type = e.layerType;
+        if (type === 'marker') {
+            const latlng = e.layer.getLatLng();
+            //console.log('Marker Lat:', latlng.lat);
+            // console.log('Marker Lng:', latlng.lng);
+        }
+    };
+    //console.log("userLoacation lat", userLocation !== null ? userLocation.lat : "not lat");
+    //console.log("userLoacation lng", userLocation !== null ? userLocation.lng : "not lng");
+
+
+    //---------leaflet
+
 
     const { token, username, appuserid } = useSelector((x) => x.authReducer);
     const dispatch = useDispatch();
@@ -61,10 +145,6 @@ const CarDetail = () => {
         queryFn: getCar,
         staleTime: 0,
     });
-
-    const [center, setCenter] = useState({ lat: 42.0970, lng: 79.2353 });
-    const ZOOM_LEVEL = 9;
-    const mapRef = useRef()
 
     const [selectedDate, setSelectedDate] = useState(null);
     const [selectedDate1, setSelectedDate1] = useState(null);
@@ -160,9 +240,9 @@ const CarDetail = () => {
     const [image, setImage] = useState(null);
     const [CarId, setCarId] = useState(byCars?.data?.id ? byCars?.data?.id : "");
 
-    useEffect(()=>{
+    useEffect(() => {
         setCarId(byCars?.data?.id ? byCars?.data?.id : "");
-    },[CarId]);
+    }, [CarId]);
 
     const fileUploadHandler = (e) => {
         const file = e.target.files[0];
@@ -203,20 +283,20 @@ const CarDetail = () => {
             formData.append("ReturnLocation.Latitude", values.ReturnLocation.Latitude);
             formData.append("ReturnLocation.Longitude", values.ReturnLocation.Longitude);
             //////////////////////////////////
-           // console.log("Image-------" + formData.getAll("Image"));
-           // console.log("FullName-------" + formData.getAll("FullName"));
-           // console.log("Email-------" + formData.getAll("Email"));
-           // console.log("Number-------" + formData.getAll("Number"));
-           // console.log("Notes-------" + formData.getAll("Notes"));
-           // console.log("CarId-------" + formData.getAll("CarId"));
-           // console.log("AppUserId-------" + formData.getAll("AppUserId"));
-           // console.log("ChauffeursId-------" + formData.getAll("ChauffeursId"));
-           // console.log("PickupDate-------" + formData.getAll("PickupDate"));
-           // console.log("ReturnDate-------" + formData.getAll("ReturnDate"));
-           // console.log("PickupLocation.Latitude-------" + formData.getAll("PickupLocation.Latitude"));
-           // console.log("PickupLocation.Longitude-------" + formData.getAll("PickupLocation.Longitude"));
-           // console.log("ReturnLocation.Latitude-------" + formData.getAll("ReturnLocation.Latitude"));
-           // console.log("ReturnLocation.Longitude-------" + formData.getAll("ReturnLocation.Longitude"));
+            // console.log("Image-------" + formData.getAll("Image"));
+            // console.log("FullName-------" + formData.getAll("FullName"));
+            // console.log("Email-------" + formData.getAll("Email"));
+            // console.log("Number-------" + formData.getAll("Number"));
+            // console.log("Notes-------" + formData.getAll("Notes"));
+            // console.log("CarId-------" + formData.getAll("CarId"));
+            // console.log("AppUserId-------" + formData.getAll("AppUserId"));
+            // console.log("ChauffeursId-------" + formData.getAll("ChauffeursId"));
+            // console.log("PickupDate-------" + formData.getAll("PickupDate"));
+            // console.log("ReturnDate-------" + formData.getAll("ReturnDate"));
+            // console.log("PickupLocation.Latitude-------" + formData.getAll("PickupLocation.Latitude"));
+            // console.log("PickupLocation.Longitude-------" + formData.getAll("PickupLocation.Longitude"));
+            // console.log("ReturnLocation.Latitude-------" + formData.getAll("ReturnLocation.Latitude"));
+            // console.log("ReturnLocation.Longitude-------" + formData.getAll("ReturnLocation.Longitude"));
             //////////////////////////////////
 
             try {
@@ -290,7 +370,39 @@ const CarDetail = () => {
                                 </div>
 
                                 <div className='ReactLeafLet'>
-                                    <Map />
+                                    <div id='myLocation'>
+                                        <Button onClick={openModal}>View Location</Button>
+                                        {showModal && (
+                                            <div className="modal">
+                                                <div className="modal-content">
+                                                    <p>Want to share your location?</p>
+                                                    <div>
+                                                        <Button backgroundColor={"green"} onClick={shareLocation}>Yes</Button>
+                                                        <Button backgroundColor={"red.700"} onClick={closeModal}>No</Button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className='ss'>
+                                        <MapContainer center={userLocation === null ? [40.3798, 49.8486] : userLocation} zoom={13} scrollWheelZoom={false} ref={mapRef}>
+                                            <TileLayer
+                                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                                url="https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=S3UF58mBkVoHt2UkKpEL"
+                                            />
+                                            <FeatureGroup>
+                                                <EditControl position='topright' onCreated={handleShapeCreated} draw={{ rectangle: false, circlemarker: false, polygon: false }} />
+                                            </FeatureGroup>
+                                            {userLocation && (
+                                                <Marker
+                                                    position={userLocation}
+                                                    icon={markerIcon}
+                                                >
+                                                    <Popup>My Location</Popup>
+                                                </Marker>
+                                            )}
+                                        </MapContainer>
+                                    </div>
                                     <div className='ChauferrsShop'>
                                         {chaurffers?.data?.slice(0, 2).map((chauf, index) => (
                                             <ChauffeursCard key={index} Id={chauf?.id} name={chauf?.name} price={chauf?.price} />
