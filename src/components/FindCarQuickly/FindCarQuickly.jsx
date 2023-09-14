7e25389f9f4e96979dcac99d30473d1e5ffa2706
Kerimov056 <ulvisk@code.edu.az>
2023-09-14 14:14:07 +0400

    FindCarQuickly page next

--- src/components/FindCarQuickly/FindCarQuickly.jsx
+++ src/components/FindCarQuickly/FindCarQuickly.jsx
@@ -8,8 +8,24 @@ const FindCarQuickly = () => {
         <>
             <div id='FindCarQuickly'>
                 <div className='FilterFindACar'>
-                    <div></div>
-                    <div></div>
+                    <div className='CityFindACarSearch'>
+
+
+                        <div class="input-wrapperFilterACar">
+                            <button class="iconFindCar">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="25px" width="25px">
+                                    <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#fff" d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"></path>
+                                    <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#fff" d="M22 22L20 20"></path>
+                                </svg>
+                            </button>
+                            <input placeholder="search.." class="inputCarS" name="text" type="text" />
+                        </div>
+
+
+                    </div>
+                    <div className='ACArFilter'>
+
+                    </div>
                 </div>
                 <div className='ResponseACar'>
 
