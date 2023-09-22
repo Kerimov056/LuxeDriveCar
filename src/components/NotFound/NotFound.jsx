dff97dbae2e23babb88f07099c9c1e8a9cc4e8aa
Kerimov056 <ulvisk@code.edu.az>
2023-09-23 01:55:35 +0400

    Not Found page next

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -3,10 +3,16 @@ import './NotFound.scss'
 
 const NotFound = () => {
     return (
-        <div className="not-found">
-            <h1>404 - Sayfa Bulunamadı</h1>
-            <p>Üzgünüz, aradığınız sayfa mevcut değil.</p>
-        </div>
+        <>
+            <div className='NotFound'>
+                <div className='NotFound_image'>
+                    <img src={"https://i.etsystatic.com/6210335/r/il/ba56c9/1679635882/il_1080xN.1679635882_sm9d.jpg"} />
+                    <div className='NotFound_Text'>
+                        <h1>404</h1>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
