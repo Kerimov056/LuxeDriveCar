e82d886fa394346c1d238cf4b0eb91134446ac0b
Kerimov056 <ulvisk@code.edu.az>
2023-09-23 01:58:38 +0400

    Not Found page Finsh

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -9,6 +9,7 @@ const NotFound = () => {
                     <img src={"https://i.etsystatic.com/6210335/r/il/ba56c9/1679635882/il_1080xN.1679635882_sm9d.jpg"} />
                     <div className='NotFound_Text'>
                         <h1>404</h1>
+                        <h2>We are sorry, the page you requested cannot be found.</h2>
                     </div>
                 </div>
             </div>
