4f0626c3df2a5f7e1aefc11ca5d65c43cf379fbd
Kerimov056 <ulvisk@code.edu.az>
2023-09-23 06:03:09 +0400

    Tripes next && ByTrip  next

--- src/components/Trips/ByTrip.jsx
+++ src/components/Trips/ByTrip.jsx
@@ -1,20 +1,42 @@
 import React from 'react'
 import './ByTrip.scss'
 import Maps from "../Map/Maps";
+import Navbar from "../Navbar/Navbar";
+import { Button } from '@chakra-ui/react';
+import { RiUserShared2Line } from "react-icons/ri";
 
 const ByTrip = () => {
     return (
         <>
+            <div style={{ marginTop: "70px" }}>
+                <Navbar />
+            </div>
             <div id='ByTrip'>
                 <div className='ByTrip_Text'>
                     <div>
                         <div className='ByTrip_Text_Main'>
-                            <div></div>
-                            <div></div>
-                            <div></div>
+                            <div className='ByTrip_Text_Main_1'>
+                                <div><Button>{"<"} Your Trips</Button></div>
+                                <div>
+                                    <Button><RiUserShared2Line /></Button>
+                                    <Button>...</Button>
+                                </div>
+                            </div>
+                            <div className='ByTrip_Text_Main_2'>
+                                <div>
+                                    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmM5MIeMxFn6wQ7OWmBG7jFAK0zW3G2bgVcg&usqp=CAU' />
+                                </div>
+                                <div>
+                                    <h1>Los Angles Trip</h1>
+                                    <h2>Sat,Sep 23 - Wed, Sep 27</h2>
+                                    <h2>Los Angles, CA</h2>
+                                </div>
+                            </div>
+                            <div className='ByTrip_Text_Main_3'>
+                                <h2>Itinerary</h2>
+                            </div>
                         </div>
                         <div className='ByTrip_Text_hed'>
-
                         </div>
                     </div>
                 </div>
