3fcdc0f2c0c9cdb68c7e6c38a2b403cee239fa96
Kerimov056 <ulvisk@code.edu.az>
2023-09-23 04:32:06 +0400

    Tripes next  &  Trips_Main

--- src/components/Trips/Trips.jsx
+++ src/components/Trips/Trips.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import './Trips.scss'
-import { Container } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 
 const Trips = () => {
     return (
         <>
-            <Container id='Trips'>
-                
-            </Container>
+            <div id='Trips'>
+                <div>
+                    <div className='Trips_Main'>
+                        <div className='Trips_Main_one'>
+                            <h1>Trips</h1>
+                            <Button>Create Trip</Button>
+                        </div>
+                        <div className='Trips_Main_two'>
+                            <div>
+                                <h1>0</h1>
+                                <h3>Days on the road</h3>
+                            </div>
+                            <div>
+                                <h1>0</h1>
+                                <h3>Miles flown</h3>
+                            </div>
+                            <div>
+                                <h1>0</h1>
+                                <h3>Cities visited</h3>
+                            </div>
+                            <div className='nextAllTripDesc'>{">"}</div>
+                        </div>
+                        <div className='Trips_Main_three'>
+                            <h2>Upcoming {"("}4{")"}</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
